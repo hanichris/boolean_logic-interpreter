bfa67a983afdb59eed8dd9c321c3b11a84fb6ca3
hanichris <hanichris71@gmail.com>
2023-01-23 21:45:10 +0300

    Entry point of the program

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - Entry point.
+ * @argc: Number of command line arguments passed to the function.
+ * @argv: NULL terminated array of strings holding command line args.
+ * Return: Exit_success.
+*/
+int main(int __attribute__ ((unused)) argc, char __attribute__ ((unused)) **argv)
+{
+    /* Initialize: Load config files if any. */
+
+    /* Interpret: Run command loop */
+	sh_loop();
+
+    /* Terminate: Perform any shutdown/cleanup */
+	return (EXIT_SUCCESS);
+}
