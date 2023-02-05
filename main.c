c21834a5bc202ab86711b6cc026cf393e2067ec6
hanichris <hanichris71@gmail.com>
2023-02-05 21:23:44 +0300

    use (void) to ignore an unused function parameter.

--- main.c
+++ main.c
@@ -8,8 +8,9 @@
  * @argv: NULL terminated array of strings holding command line args.
  * Return: Exit_success.
 */
-int main(int __attribute__ ((unused)) argc, char __attribute__ ((unused)) **argv)
+int main(int __attribute__ ((unused)) argc, char **argv)
 {
+	(void)argv;
     /* Initialize: Load config files if any. */
 
     /* Interpret: Run command loop */
