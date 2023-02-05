de0950d427dc267f601375a4e4ffa8fd9f88b299
hanichris <hanichris71@gmail.com>
2023-02-05 23:59:16 +0300

    Header file holding function prototypes that operate on stack data structure.

---
+++ _stack_.h
@@ -0,0 +1,30 @@
+#ifndef _STACK_HEADER_
+#define _STACK_HEADER_
+
+#include <sys/queue.h>
+
+/**
+ * struct node - singly linked list representation of a stack.
+ * @s: pointer to a character array.
+ * @nodes: pointer to the nodes in the linked list.
+ *
+ * Description: Singly linked list node structure for stack,
+ * LIFO.
+*/
+typedef struct node
+{
+	char *s;
+
+	SLIST_ENTRY(node) nodes;
+} node_t;
+
+/* For ease of pointer assignment to `head_t` without compiler error. */
+typedef SLIST_HEAD(stack, node) head_t;
+
+void push(head_t *head, char *s);
+char *pop(head_t *head);
+int isEmpty(head_t *head);
+void free_stack(head_t *head);
+
+
+#endif /* _STACK_HEADER_ */
