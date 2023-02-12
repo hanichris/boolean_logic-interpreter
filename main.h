b1d7c67c00f367aaf9f3b56467ce93b1d18d0c70
hanichris <hanichris71@gmail.com>
2023-02-12 22:34:05 +0300

    include the hashmap header file and eval_postfix function prototype.

--- main.h
+++ main.h
@@ -1,6 +1,9 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
 
+#include "_hash_.h"
+
+char *eval_postfix(char **postfix, hash_table_t *ht);
 char **infix_2_postfix(char **args);
 char *read_line(void);
 char **split_line(char *line);
